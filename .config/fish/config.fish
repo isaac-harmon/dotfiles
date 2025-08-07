de9df517902a685ae23f4d8998b65e47b41fcbb9
Isaac Harmon <isharmon@proton.me>
2025-08-07 00:40:43 -0700

    Fixed cd alias by turning it into a function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,14 @@ if status is-interactive
 
 	# Enhanced navigation
 	zoxide init fish | source
-	alias cd='z && ls'
+	function cd --argument dir
+		if [ "dir" = "" ]
+			z $HOME
+		else
+			z $dir
+		end
+		ls
+	end
 
 	alias ls='ls --color --group-directories-first'
 	

68aac20af56ebec001e57848b9478df894ba6f06
Isaac Harmon <isharmon@proton.me>
2025-08-06 21:50:22 -0700

    Added fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+
+	# Enhanced navigation
+	zoxide init fish | source
+	alias cd='z'
+
+	alias ls='ls --color --group-directories-first'
+	
+	set -gx $EDITOR nvim
+	alias vi='nvim'
+	alias vim='nvim'
+
+	# Prompt
+	starship init fish | source
+
+	# Yazi navigation shortcut
+	function y
+		set tmp (mktemp -t "yazi-cwd.XXXXXX")
+		yazi $argv --cwd-file="$tmp"
+		if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+			builtin cd -- "$cwd"
+		end
+		rm -f -- "$tmp"
+	end
+
+	# Startup script
+	set -g fish_greeting
+	fastfetch
+	echo
+	ls
+end

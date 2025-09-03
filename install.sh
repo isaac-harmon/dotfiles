4144c58abba7fdc563e0a83101ea9d87ab321b75
Isaac Harmon <isharmon@pm.me>
2025-09-02 22:10:24 -0700

    Cleaned up script output

--- install.sh
+++ install.sh
@@ -24,22 +24,51 @@ done
 
 # Installing packages
 echo
+echo --- Installing Lazygit repository ---
+echo
 sudo dnf copr enable dejan/lazygit
+
+echo
+echo --- Installing Starship repository ---
 echo
 sudo dnf copr enable atim/starship
+
+echo
+echo --- Installing Yazi repository ---
 echo
 sudo dnf copr enable lihaohong/yazi
+
+echo
+echo --- Installing packages ---
 echo
 sudo dnf install fastfetch foot git lazygit neovim starship stow yazi zoxide zsh
 
 # Cloning Git repos
+echo
+echo --- Cloning dotfiles repository ---
+echo
+
 git clone https://github.com/isaac-harmon/dotfiles.git ~/.dotfiles
+
+
 mkdir ~/.dotfiles/.config/zsh
+
+echo
+echo --- Cloning Zsh Autosuggestions repository ---
+echo
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.dotfiles/.config/zsh/zsh-autosuggestions
+
+echo
+echo --- Cloning Zsh Syntax Highlighting repository ---
+echo
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/.config/zsh/zsh-syntax-highlighting
 
 # Managing symlinks
+mv ~/.bashrc ~/.bashrc.bak
+mv ~/.zshrc ~/.zshrc.bak
 stow ~/.dotfiles
+
+# Deleting unnesseccary symlinks
 rm ~/install-fedora.sh
 rm ~/Assets
 
